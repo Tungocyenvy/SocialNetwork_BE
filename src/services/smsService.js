4486fa288e0137bee180383760d9f8fb496e9456
gaixuongrong <vytungocyen@gmail.com>
2022-02-22 12:44:55 +0700

    forgotPassword

--- src/services/smsService.js
+++ src/services/smsService.js
@@ -1,10 +1,11 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+const from = process.env.TWILIO_PHONE_NUMBER;
 const Twilio = require('twilio')(accountSid, authToken);
 
 function sendSMS(to, body) {
   Twilio.messages
-    .create({ to, body })
+    .create({ to, from, body })
     .then((message) => {
       console.log('SMS message successful!');
     })
